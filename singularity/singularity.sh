317ec87996ce9365164ed73ac59ced88c524edaf
Evan Shockley <ershockley@uchicago.edu>
2021-03-29 12:42:54 -0500

    update singularity example

--- singularity/singularity.sh
+++ singularity/singularity.sh
@@ -3,6 +3,10 @@
 . /opt/XENONnT/setup.sh
 
 export RUCIO_ACCOUNT=xenon-analysis
-
+export XENON_CONFIG=$PWD/.xenon_config
 rucio whoami
 
+python -c "import strax; print(strax.__file__)"
+
+python -c "from utilix import db"
+q
